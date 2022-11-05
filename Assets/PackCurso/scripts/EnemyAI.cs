be3dc0a39ed8feb080ce80c8a5a2434505ab424c
allysh0w <allyson.martinz@gmail.com>
2022-11-05 10:14:20 -0300

    added enemy ai

---
+++ Assets/PackCurso/scripts/EnemyAI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Transform playerTransform; 
+    private Transform enemyTransform; 
+    public float speed = 3f;
+    private SpriteRenderer spriteRenderer;
+    private EnemyAttack enemyAttack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Fall_0").GetComponent<Transform>();
+        enemyTransform = GetComponent<Transform>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        enemyAttack = GetComponent<EnemyAttack>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemyAttack.playerInRange){
+
+        } else {
+            ChasePlayer();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        var playerIsToRight = enemyTransform.position.x < playerTransform.position.x;
+
+        if(playerIsToRight == true)
+        {   
+            enemyTransform.Translate(Vector3.right * Time.deltaTime * speed);
+            spriteRenderer.flipX = false;
+        }else{
+            enemyTransform.Translate(Vector3.left * Time.deltaTime * speed);
+            spriteRenderer.flipX = true;
+        }
+    }
+}
